171c25b3b79340abbd97ef5a76dd6fb4af57108f
igorkan <igorkan@users.noreply.github.com>
2014-07-28 01:40:50 +0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Prg assignment #2
+## by Igor Kanovsky (ik)
 
-## Write a short comment describing this function
+
+## function input is an inversible matrix
+## returns list: the same matrix with set of functions for its cashable inversion
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse )
+     
 }
 
 
-## Write a short comment describing this function
+## input is a matrix created by makeCacheMatrix()
+## returns the inverse matrix wich will be retrived from cash after first calculation
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
+   
 }
